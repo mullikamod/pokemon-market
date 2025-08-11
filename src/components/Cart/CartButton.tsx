86c3355d7f3ac20adf08af66b4b8cae4a48c49fe
Mullika <mullika.modd@gmail.com>
2025-08-11 14:12:28 +0700

    💄: add ui totle qty on cart button

--- src/components/Cart/CartButton.tsx
+++ src/components/Cart/CartButton.tsx
@@ -1,20 +1,34 @@
 "use client";
 
-
 import { ShoppingBag } from "lucide-react";
 import Button from "../Button/Button";
+import { cartTotals, useCartStore } from "@/store/cartStore";
 
 type CartButtonProps = {
   onClick: () => void;
 };
 
-const CartButton = ({ onClick }: CartButtonProps) => {
+const CartButton: React.FC<CartButtonProps> = ({ onClick }) => {
+  const cartLines = useCartStore((state) => state.lines);
+  const { totalQuantity } = cartTotals(cartLines);
+
+  const handleClick = () => {
+    onClick();
+  };
 
   return (
-    <Button onClick={onClick}>
-      <ShoppingBag className="w-6 h-6 text-white" />
-    </Button>
+    <div className="relative">
+      <Button onClick={handleClick}>
+        <ShoppingBag className="w-6 h-6 text-white" />
+      </Button>
+
+      {totalQuantity > 0 && (
+        <span className="absolute -top-1 -right-1 bg-white btn-glow text-primary text-xs font-bold rounded-full px-2 py-0.5">
+          {totalQuantity}
+        </span>
+      )}
+    </div>
   );
-}
+};
 
 export default CartButton;
